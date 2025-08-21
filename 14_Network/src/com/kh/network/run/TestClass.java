4593dcfeed67356ded2b278a41484af76eedeae2
Taeho Kim <kth8631@gmail.com>
2025-08-21 11:07:54 +0900

    Modify TestClass

--- 14_Network/src/com/kh/network/run/TestClass.java
+++ 14_Network/src/com/kh/network/run/TestClass.java
@@ -3,8 +3,11 @@ package com.kh.network.run;
 public class TestClass {
 
 	public static void main(String[] args) {
+		// 얘는 깜장이
 		
 		// 이번엔 진짜로 확정지어볼거임
+		// Home Sweet Home ~ ~ ~
+		// 집 좋다 좋아 ~~ 이게 집이지 ~~
 
 
 	}
